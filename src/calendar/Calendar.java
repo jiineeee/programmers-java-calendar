e34f1ae1bff1522f420d56ebcd34893f9a2b256b
jiine <jiine@192.168.1.106>
2018-05-15 16:36:44 +0900

    Update Calendar

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -4,29 +4,50 @@ import java.util.Scanner;
 
 public class Calendar {
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
+	private static final String[] WEEK_DAYS = {"SU", "MO", "TU", "WE", "TH", "FR", "SA"};
 	public int getMaxDaysOfMonth(int month) {
 		return MAX_DAYS[month - 1];
 	}
-
+	
+	public void printCalendar(int year, int month, String day) {
+		System.out.printf("<<%d년 %d월>>\n",year,month);
+		System.out.println("SU MO TU WE TH FR SA");
+		System.out.println("--------------------");
+		int maxDay = getMaxDaysOfMonth(month);
+		if((year%4==0 && year%100!=0)||year%400==0) maxDay = 29;
+		int dayNum = 0;
+		for(int i=0;i<WEEK_DAYS.length;i++) {
+			if(WEEK_DAYS[i].equals(day)) {
+				dayNum = i;
+			}
+		}
+		for(int j=0;j<dayNum;j++) {
+			System.out.print("   ");
+		}
+		for(int i=1;i<=maxDay;i++) {
+			if(i<10) {
+				System.out.print(i + "  ");
+			}else {
+				System.out.print(i + " ");
+			}
+			if((i+dayNum)%7==0) {
+				System.out.println();
+			}
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		// 숫자를 입력 받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
 		Calendar c = new Calendar();
-		int month = 0;
-		while(true) {
-			System.out.println("월을 입력하세요.");
-			month = scanner.nextInt();
-			if(month==-1) break;
-			System.out.println("일\t월\t화\t수\t목\t금\t토");
-			for(int i=1;i<=c.getMaxDaysOfMonth(month);i++) {
-				System.out.print(i + "\t");
-				if(i%7==0) {
-					System.out.println();
-				}
-			}
-			System.out.println();
-		}
-		System.out.println("Bye~");
+		System.out.print("년도를 입력하세요.\nYEAR> ");
+		int year = scanner.nextInt();
+		System.out.print("달을 입력하세요.\nMONTH> ");
+		int month = scanner.nextInt();
+		System.out.println("첫번째 요일을 입력하세요.(SU, MO, TU, WE, TH, FR, SA)");
+		System.out.print("WEEKDAY> ");
+		String day = scanner.next();
+		c.printCalendar(year, month, day);
 	}
 }

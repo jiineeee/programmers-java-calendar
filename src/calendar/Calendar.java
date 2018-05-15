bfa473fa495e5620495180cd13772d0d36da33a2
jiine <jiine@192.168.1.106>
2018-05-15 16:02:13 +0900

    Print Calendar

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -13,17 +13,20 @@ public class Calendar {
 		// 숫자를 입력 받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
 		Calendar c = new Calendar();
-		System.out.println("반복 횟수를 입력하세요.");
-		int count = scanner.nextInt();
-
-		System.out.println("월을 입력하세요.");
-		int[] month = new int[count];
-		for (int i = 0; i < count; i++) {
-			month[i] = scanner.nextInt();
+		int month = 0;
+		while(true) {
+			System.out.println("월을 입력하세요.");
+			month = scanner.nextInt();
+			if(month==-1) break;
+			System.out.println("일\t월\t화\t수\t목\t금\t토");
+			for(int i=1;i<=c.getMaxDaysOfMonth(month);i++) {
+				System.out.print(i + "\t");
+				if(i%7==0) {
+					System.out.println();
+				}
+			}
+			System.out.println();
 		}
-		for (int i = 0; i < count; i++) {
-			System.out.println(month[i] + "월은 " + c.getMaxDaysOfMonth(month[i]) + "일까지 있습니다.");
-
-		}
+		System.out.println("Bye~");
 	}
 }

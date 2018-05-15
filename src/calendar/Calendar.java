df431c795ec6cb7bd203aa77a5a63698a6f00edf
jiine <jiine@192.168.1.106>
2018-05-15 14:09:37 +0900

    Update calendar main()

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -2,6 +2,13 @@ package calendar;
 
 public class Calendar {
 	public static void main(String[] args) {
-		System.out.println("캘린더");
+		System.out.println("일\t월\t화\t수\t목\t금\t토");
+		System.out.println("---------------------------------------------------");
+		for (int i = 1; i <= 28; i++) {
+			System.out.print(i + "\t");
+			if (i % 7 == 0) {
+				System.out.println();
+			}
+		}
 	}
 }
